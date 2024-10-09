71c5868883290ee38a9cdd5060e664656b047936
ROUX SACHA <sacha.work38@gmail.com>
2024-10-09 13:09:28 +0200

    CSS de la page choixmot.php

--- public/choixmot.php
+++ public/choixmot.php
@@ -1,13 +1,44 @@
 <?php
     require_once('header.php');
 ?>
-<form method="post">
-    <label for="word">Entrez le mot à deviner :</label>
-    <input type="text" name="word">
-    <label for="joueur2">Nombre de coups :</label>
-    <input type="number" name="shot">
-    <button type="submit" class="button">Continuer</button>
-</form>
+<div class='mainContainer'>
+    <div class ='playerForm'>
+        <h2>Paramètres</h2>
+        <form method="post">
+            <label for="word">Entrez le mot à deviner :</label>
+            <input type="text" name="word">
+            <label for="joueur2">Nombre de coups :</label>
+            <input type="number" name="shot">
+            <button type="submit" class="button">Continuer</button>
+        </form>
+    </div>
+    <div class='scoreBoard'>
+        <h2>Tableau des anciennes parties</h2>
+        <table>
+            <tr class='defaultLine'>
+                <td>Joueur1</td>
+                <td>Joueur2</td>
+                <td>Mot à trouver</td>
+                <td>Nombre de coups</td>
+                <td>Gagnant</td>
+            </tr>
+            <?php
+                $connection = getConnection();
+                $sql = "Select * from partie order by id_game desc";
+                foreach ($connection ->query($sql) as $row) {
+                    echo"<tr>
+                            <td>{$row['player1']}</td>
+                            <td>{$row['player2']}</td>
+                            <td>{$row['word']}</td>
+                            <td>{$row['shot']}</td>
+                            <td>{$row['winner']}</td>
+                        </tr>";
+                }
+            ?>
+        </table>
+    </div>
+</div>
+
 <?php
    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     if (!empty($_POST['word']) && !empty($_POST['shot'])){
